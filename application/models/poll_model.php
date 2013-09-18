0db0995497c8686a37dfa5d0b734db2a68cbe186
India Meisner <india.rae@gmail.com>
2013-09-17 21:17:59 -0700

    Updated CSS to display poll info a little nicer. Updated poll model to declare function that we will need for updating votes. Updated polls_all.php view so that the Add Poll button correctly loads add_poll.php view. polls_all view also lists information about all polls (still needs work). This is the two-hour mark, must stop now and continue tomorrow. TODO: create database model(s) for poll options, to keep track of votes... did not initially consider that. Also need functions to count/calculate votes. Not close to AJAX functionality yet.

--- application/models/poll_model.php
+++ application/models/poll_model.php
@@ -29,18 +29,10 @@ class Poll_model extends CI_model {
         return $this->db->insert('polls', $this);
     }
 
-    // function update_entry()
-    // {
-    //     $this->title		= $_POST['title'];
-    //     $this->description	= $_POST['description'];
-    //     $this->option1		= $_POST['option1'];
-    //     $this->option2		= $_POST['option2'];
-    //     $this->option3		= $_POST['option3'];
-    //     $this->option4		= $_POST['option4'];
-    //     $this->updated_at	= time();
-
-    //     $this->db->update('entries', $this, array('id' => $_POST['id']));
-    // }
+    function process_vote()
+    {
+        echo "This is the process_vote function!";
+    }
 
 }
 
--- application/views/polls_all.php
+++ application/views/polls_all.php
@@ -1,24 +1,37 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Poll List</title>
-		<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<!-- button to add poll -->
-		<a href="#">Add a Poll</a>
-		<!-- id number of poll -->
-		<?php
-			foreach ($polls as $poll)
-			{
-				// echo "<pre>";
-				// var_dump($user);
-				// echo "</pre>";
-
-		?>
-				<h2>ID: <?= $poll->id ?></h2>
-		<?php	}
-		?>
-	</body>
+<head>
+	<meta charset="UTF-8">
+	<title>Poll List</title>
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="../assets/css/mycss.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="wrapper" class="container">
+		<!-- TODO: Add some top/left margin around Add button! -->
+		<a href="poll/add" class="btn" id="add_redirect">Add a Poll</a>
+<?php
+		foreach ($polls as $poll)
+		{
+			// echo "<pre>";
+			// var_dump($user);
+			// echo "</pre>";
+?>
+			<div class="container">
+				<p class="pull-right">ID: <?= $poll->id ?></p>
+				<div class="well">
+					<h4><?= $poll->title ?></h4>
+					<p><?= $poll->description ?></p>
+					<form id="poll_<?= $poll->id ?>" action="poll/process_vote" method="post">
+						<!-- radio buttons for options -->
+						<button type="submit" class="btn btn-primary pull-right">submit</button>
+					</form>
+				</div>
+				<div id="results_poll_<?= $poll->id ?>">
+					<!-- call function to calc vote results -->
+				</div>
+			</div>
+<?php	}?>
+	</div>
+</body>
 </html>
